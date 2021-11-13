b363c456d401ee76df022787897941f67fd849b6
jiikko <n905i.1214@gmail.com>
2021-11-13 14:02:04 +0900

    order

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,5 +1,5 @@
 class Admin::EventsController < Admin::Base
   def index
-    @events = Event.all.page(params[:page])
+    @events = Event.all.page(params[:page]).order(id: :desc)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   namespace :api do
     resources :events, only: [:create]
   end
+
+  namespace :admin do
+    resources :events, only: [:index]
+  end
 end
