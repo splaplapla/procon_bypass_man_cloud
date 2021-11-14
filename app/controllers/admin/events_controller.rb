92fb84729ca20f3b13cb426e3c2ae05662812ccb
jiikko <n905i.1214@gmail.com>
2021-11-14 19:05:40 +0900

    modoru

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -2,8 +2,11 @@ class Admin::EventsController < Admin::Base
   def index
     @events = Event.all
     if params[:pbm_session_id]
-      @events = @events.where(pbm_session_id: params[:pbm_session_id])
+      pbm_session = PbmSession.find(params[:pbm_session_id])
+      @events = @events.where(pbm_session_id: pbm_session)
+      @device = pbm_session.device
     end
+
     @events = @events.page(params[:page]).order(id: :desc)
   end
 end
--- app/models/pbm_session.rb
+++ app/models/pbm_session.rb
@@ -1,3 +1,5 @@
 class PbmSession < ApplicationRecord
   has_many :events, dependent: :destroy
+
+  belongs_to :device
 end
