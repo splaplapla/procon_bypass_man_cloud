f8148433ad501fc74d48a1792ae857af70e0f172
jiikko <n905i.1214@gmail.com>
2021-11-20 13:16:38 +0900

    filter

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,12 +1,15 @@
 class Admin::EventsController < Admin::Base
   def index
     @events = Event.all
-    if params[:pbm_session_id]
-      pbm_session = PbmSession.find(params[:pbm_session_id])
-      @events = @events.where(pbm_session_id: pbm_session)
-      @device = pbm_session.device
+    if event_params[:event_type].present?
+      @events = @events.where(event_type: event_params[:event_type])
     end
-
     @events = @events.page(params[:page]).order(id: :desc)
+  end
+
+  private
+
+  def event_params
+    params.required(:event).permit(:event_type)
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
   serialize :body, JSON
 
   belongs_to :pbm_session
+
+  def self.event_types
+    [ OpenStruct.new(name: :error),
+      OpenStruct.new(name: :boot),
+      OpenStruct.new(name: :load_config),
+      OpenStruct.new(name: :reload_config),
+    ]
+  end
 end
