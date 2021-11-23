059f7d73014edde3edb5cd04a3b75dd1541e4e4d
jiikko <n905i.1214@gmail.com>
2021-11-24 02:25:24 +0900

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     resources :events, only: [:create]
+
     resources :devices, only: :show do
       resources :pbm_jobs, only: :index
     end
@@ -12,11 +13,13 @@ Rails.application.routes.draw do
     resources :events, only: [:index] do
       resources :saved_buttons_settings, only: [:create]
     end
+
     resources :devices, only: [:index, :show] do
       resource :device_versions, only: [:show] do
         get :current, on: :member
         post :change_request, on: :member
       end
+
       resources :saved_buttons_settings, only: [:index, :show]
       resources :pbm_sessions, only: [:index, :show] do
       end
