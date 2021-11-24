40bb5b0837a930f417f4b89d23a269299459a69e
jiikko <n905i.1214@gmail.com>
2021-11-24 13:33:57 +0900

    add condition

--- app/controllers/admin/pbm_jobs.rb
+++ app/controllers/admin/pbm_jobs.rb
@@ -1,9 +1,9 @@
 class Admin::PbmJobs < Admin::Base
   before_action :require_empty_pending_job
 
-  def has_no_pending_job
+  def require_empty_pending_job
     @device = Device.find(params[:device_id])
-    if @device.pbm_jobs.queued.present?
+    if @device.pbm_jobs.queued.present? || @device.pbm_jobs.in_progress.present?
       redirect_to admin_device_path(@device), notice: "既に実行待ちのジョブがあるのでリクエストの作成は中止しました。"
     end
   end
