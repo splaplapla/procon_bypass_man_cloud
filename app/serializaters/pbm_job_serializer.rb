0648494b4afa2b93426bcbb1d0ae881f3f0f5dc0
jiikko <n905i.1214@gmail.com>
2021-11-25 01:04:40 +0900

    created_atも返す

--- app/serializaters/pbm_job_serializer.rb
+++ app/serializaters/pbm_job_serializer.rb
@@ -1,15 +1,20 @@
 class PbmJobSerializer
   include ActiveModel::Serializers::JSON
 
-  attr_accessor :action, :status, :uuid
+  attr_accessor :action, :status, :uuid, :created_at
 
   def initialize(pbm_job)
     self.action = pbm_job.action
     self.status = pbm_job.status
     self.uuid = pbm_job.uuid
+    self.created_at = pbm_job.created_at
   end
 
   def attributes
-    { action: action, status: status, uuid: uuid }
+    { action: action,
+      status: status,
+      uuid: uuid,
+      created_at: created_at,
+    }
   end
 end
