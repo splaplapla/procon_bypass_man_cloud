7f65922947b5e5bff088c099ccae4f4e2f7bd3c7
jiikko <n905i.1214@gmail.com>
2021-11-30 21:34:05 +0900

    バージョンを送信できるようにする

--- app/controllers/admin/pbm_jobs/change_pbm_version_controller.rb
+++ app/controllers/admin/pbm_jobs/change_pbm_version_controller.rb
@@ -1,7 +1,7 @@
 class Admin::PbmJobs::ChangePbmVersionController < Admin::PbmJobs
   def create
     device = Device.find(params[:device_id])
-    Device::ChangeVersionRequestService.new(device: device).execute(next_version: params[:next_version])
+    Device::ChangeVersionRequestService.new(device: device).execute(next_version: params[:pbm_version])
 
     redirect_to admin_device_path(@device), notice: "アクションを実行リクエストを作成しました"
   rescue Device::ChangeVersionRequestService::NeedPbmenvError
--- app/models/support_pbm_version.rb
+++ app/models/support_pbm_version.rb
@@ -1,8 +1,9 @@
 class SupportPbmVersion
   def self.versions
     %w(
+      0.1.13
       0.1.12
-      0.1.1
+      0.1.11
       0.1.10
       0.1.9
     )
