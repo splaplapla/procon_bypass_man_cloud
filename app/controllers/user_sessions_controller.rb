0d0bc101139f5e7d76b6a2d638df309cc67d1c1b
jiikko <n905i.1214@gmail.com>
2021-12-05 01:04:55 +0900

    add devices index page

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -8,7 +8,7 @@ class UserSessionsController < ApplicationController
     @user = login(params[:email], params[:password])
 
     if @user
-      redirect_back_or_to(:users, notice: 'Login successful')
+      redirect_back_or_to(:devices, notice: 'Login successful')
     else
       flash.now[:alert] = 'Login failed'
       render action: 'new'
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get 'login' => 'user_sessions#new', as: :login
   post 'login' => "user_sessions#create"
   post 'logout' => 'user_sessions#destroy', as: :logout
+  resources :devices, only: [:index, :show, :edit]
 
   namespace :api do
     resources :events, only: [:create]
