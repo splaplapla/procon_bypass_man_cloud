5c7ad030c754f40a80f298c316c265f2aee98c1c
jiikko <n905i.1214@gmail.com>
2021-12-05 01:08:14 +0900

    add spec

--- spec/requests/user_sessions_spec.rb
+++ spec/requests/user_sessions_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "UserSessions", type: :request do
   describe "GET /new" do
-    it "returns http success" do
+    it do
       get login_path
       expect(response).to be_ok
     end
