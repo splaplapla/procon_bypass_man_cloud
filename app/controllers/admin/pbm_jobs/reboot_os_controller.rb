dddd75d9c0769f718d72bd560614460db51b623d
jiikko <n905i.1214@gmail.com>
2021-12-11 01:46:34 +0900

    oooooooooops

--- app/controllers/admin/pbm_jobs/reboot_os_controller.rb
+++ app/controllers/admin/pbm_jobs/reboot_os_controller.rb
@@ -1,8 +1,8 @@
 class Admin::PbmJobs::RebootOsController < Admin::PbmJobs
   def create
-    @device = Device.find(params[:device_id])
-    pbm_job = Admin::PbmJob::CreateRebootOsService.new(device: @device).execute!
+    device = Device.find(params[:device_id])
+    pbm_job = Admin::PbmJob::CreateRebootOsService.new(device: device).execute!
     ActionCable.server.broadcast(device.push_token, PbmJobSerializer.new(pbm_job).attributes)
-    redirect_to admin_device_path(@device), notice: "アクションを実行リクエストを作成しました"
+    redirect_to admin_device_path(device), notice: "アクションを実行リクエストを作成しました"
   end
 end
--- app/controllers/admin/pbm_jobs/stop_pbm_controller.rb
+++ app/controllers/admin/pbm_jobs/stop_pbm_controller.rb
@@ -1,14 +1,14 @@
 # 使っていない
 class Admin::PbmJobs::StopPbmController < Admin::PbmJobs
   def create
-    @device = Device.find(params[:device_id])
+    device = Device.find(params[:device_id])
 
     pbm_job = PbmJobFactory.new(
-      device_id: @device.id,
+      device_id: device.id,
       action: :stop_pbm,
     ).build
     pbm_job.save!
 
-    redirect_to admin_device_path(@device), notice: "アクションを実行リクエストを作成しました"
+    redirect_to admin_device_path(device), notice: "アクションを実行リクエストを作成しました"
   end
 end
