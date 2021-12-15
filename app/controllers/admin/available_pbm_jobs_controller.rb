1806177aec31f062049570e913af2f176ed1fdb1
jiikko <n905i.1214@gmail.com>
2021-12-15 23:30:58 +0900

    選択可能なバージョンにひとことを足す

--- app/controllers/admin/available_pbm_jobs_controller.rb
+++ app/controllers/admin/available_pbm_jobs_controller.rb
@@ -1,5 +1,12 @@
 class Admin::AvailablePbmJobsController < Admin::Base
   def index
     @device = Device.find(params[:device_id])
+    @selectable_pbm_versions = SupportPbmVersion.versions.map do |version|
+      if @device.pbm_version == version
+        "#{version}(now)"
+      else
+        version
+      end
+    end
   end
 end
