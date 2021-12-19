e20dd096234ed5c1e48607321ca141b9d869c45a
jiikko <n905i.1214@gmail.com>
2021-12-19 13:43:26 +0900

    fix

--- app/controllers/admin/saved_buttons_settings/contents_controller.rb
+++ app/controllers/admin/saved_buttons_settings/contents_controller.rb
@@ -2,7 +2,11 @@ class Admin::SavedButtonsSettings::ContentsController < Admin::Base
   def edit
     @setting = SavedButtonsSetting.find(params[:id])
     @device = @setting.device
-    @config = ProconBypassMan::ButtonsSettingConfiguration.instance.instance_eval(YAML.load(@setting.content)["setting"])
+    if(setting_content = @setting.content["setting"])
+      @config = ProconBypassMan::ButtonsSettingConfiguration.instance.instance_eval(YAML.load(setting_content))
+    else
+      @config = ProconBypassMan::ButtonsSettingConfiguration.instance
+    end
   end
 
   def update
