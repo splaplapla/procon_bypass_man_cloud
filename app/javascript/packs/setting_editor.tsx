12e6513171ffea4d4d0776ba4aa0cf0764a3630c
jiikko <n905i.1214@gmail.com>
2021-12-26 20:26:22 +0900

    wrap with context

--- app/javascript/packs/setting_editor.tsx
+++ app/javascript/packs/setting_editor.tsx
@@ -6,13 +6,32 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import { ButtonsSetting } from './setting_editor/components/main'
 import { Button } from "types/button";
+import { Layers } from "types/layer";
+import { SettingContext } from "./setting_editor/setting_context";
+
+const SettingProvider = ({children}) => {
+  const initLayers: Layers = {
+    installed_modes: {},
+  }
+  const [prefixKeys, setPrefixKeys] = useState([] as Array<Button>);
+  const value = {
+    prefixKeys,
+  }
+  return (
+    <SettingContext.Provider value={value}>
+      {children}
+    </SettingContext.Provider>
+  )
+}
 
 export const ButtonsSettingPage = () => {
   const prefixKeys = JSON.parse(document.getElementById("config-prefix-keys").dataset.configPrefixKeys) as Array<Button>
 
   return(
     <>
-      <ButtonsSetting defaultPrefixKeys={prefixKeys} />
+      <SettingProvider>
+        <ButtonsSetting defaultPrefixKeys={prefixKeys} />
+      </SettingProvider>
     </>
   )
 }
