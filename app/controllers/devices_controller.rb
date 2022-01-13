73e98ad214b2d1a65d3ea3f4c30b1641785c6be7
jiikko <n905i.1214@gmail.com>
2022-01-13 22:17:44 +0900

    add show page

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -2,4 +2,8 @@ class DevicesController < ApplicationController
   def index
     @devices = current_user.devices
   end
+
+  def show
+    @device = current_user.devices.find_by(uuid: params[:id])
+  end
 end
