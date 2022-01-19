d4c7833eaa9efb0b7ca4b8a89ddc00b0f68b20cf
jiikko <n905i.1214@gmail.com>
2022-01-19 21:34:07 +0900

    default locale ja

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,8 @@ module ProconBypassManCloud
     # in config/environments, which are processed later.
     #
     config.time_zone = "Asia/Tokyo"
+
+    config.i18n.default_locale = :ja
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
