a425e94261858fbc36d3e0020e8bcf1e33a06ac9
jiikko <n905i.1214@gmail.com>
2022-01-19 21:44:13 +0900

    apiはenを固定にする

--- app/controllers/api/base.rb
+++ app/controllers/api/base.rb
@@ -1,9 +1,17 @@
 class Api::Base < ActionController::Base
   skip_before_action :verify_authenticity_token
 
+  around_action :switch_locale
+
   # TODO RecordNotFoundとか起きたらjsonを返す
 
   def get_device
     @device ||= Device.find_or_create_by!(uuid: params[:device_id])
+  end
+
+  def switch_locale(&action)
+    locale = :en
+
+    I18n.with_locale(locale, &action)
   end
 end
