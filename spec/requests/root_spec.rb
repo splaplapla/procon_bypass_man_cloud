ea6b706085053019d0c97bd2de5ffcd88e33e395
jiikko <n905i.1214@gmail.com>
2022-01-29 17:10:12 +0900

    add spec

--- spec/requests/root_spec.rb
+++ spec/requests/root_spec.rb
@@ -2,6 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "Root", type: :request do
   describe "GET /index" do
+    context 'ログインしているとき' do
+      include_context "login_with_user"
+
+      it  do
+        get root_path
+        expect(response).to be_redirect
+      end
+    end
+
     context 'ログインしていないとき' do
       it  do
         get root_path
