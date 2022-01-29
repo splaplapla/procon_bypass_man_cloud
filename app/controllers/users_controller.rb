4f137c059aebb589a9057e7eac5c5bf124ae5a74
jiikko <n905i.1214@gmail.com>
2022-01-29 16:05:01 +0900

    ユーザ新規登録画面を作成する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   skip_before_action :require_login, only: [:new, :create]
+  before_action :redirect_if_has_user, only: [:new, :create]
 
   def new
+    @user = User.new
   end
 
   def edit
@@ -19,5 +21,18 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(params.require(:user).permit(:email, :password, :password_confirmation))
+    if @user.save
+      auto_login(@user)
+      redirect_to devices_path, notice: "登録ができました"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def redirect_if_has_user
+    redirect_to devices_path if current_user
   end
 end
