7a73fec4a41aca82c67de93e30c42b2aaeae954b
jiikko <n905i.1214@gmail.com>
2022-02-17 20:37:02 +0900

    a

--- app/controllers/internal/procon_bypass_man_versions_controller.rb
+++ app/controllers/internal/procon_bypass_man_versions_controller.rb
@@ -2,6 +2,7 @@ class Internal::ProconBypassManVersionsController < ApplicationController
   skip_before_action :require_login, only: [:show]
 
   def show
+    # 問い合わせは遅いのでキャッシュする
     Rails.cache.fetch([:pbm_version], expires_in: 1.hours) do
       ProconBypassManVersion.fetch_latest_version
     end
