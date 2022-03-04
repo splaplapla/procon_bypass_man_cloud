713ebd59c93817335147b5a2f40613484bb4c1bb
jiikko <n905i.1214@gmail.com>
2022-03-04 21:11:51 +0900

    use current_user

--- app/controllers/public_saved_buttons_settings_controller.rb
+++ app/controllers/public_saved_buttons_settings_controller.rb
@@ -7,13 +7,16 @@ class PublicSavedButtonsSettingsController < ApplicationController
   end
 
   def create
-    saved_buttons_setting = SavedButtonsSetting.find(params[:saved_buttons_setting_id])
+    saved_buttons_setting = current_user.saved_buttons_settings.find(params[:saved_buttons_setting_id])
     public_saved_buttons_setting = saved_buttons_setting.create_public_saved_buttons_setting!
     redirect_to public_saved_buttons_setting_path(public_saved_buttons_setting.unique_key)
   end
 
   def destroy
-    public_saved_buttons_setting = PublicSavedButtonsSetting.find(params[:id])
+    public_saved_buttons_setting = PublicSavedButtonsSetting.find_by(
+      id: params[:id],
+      saved_buttons_setting_id: current_user.saved_buttons_settings.find(params[:saved_buttons_setting_id]).id
+    )
     public_saved_buttons_setting.destroy
     redirect_to saved_buttons_settings_path
   end
