b8b8e798dfbe4367773352ed6fe87277480ef8af
jiikko <n905i.1214@gmail.com>
2022-03-10 09:35:48 +0900

    action keyは必要

--- app/serializaters/pbm_remote_macro_job_serializer.rb
+++ app/serializaters/pbm_remote_macro_job_serializer.rb
@@ -1,8 +1,9 @@
 class PbmRemoteMacroJobSerializer
-  attr_accessor :action, :uuid, :steps, :created_at
+  attr_accessor :action, :name, :uuid, :steps, :created_at
 
   def initialize(remote_macro_job)
-    self.action = remote_macro_job.name
+    self.action = 'remote_macro' # ProconBypassMan::RemoteMacro::ACTION_KEY
+    self.name = remote_macro_job.name
     self.uuid = remote_macro_job.uuid
     self.steps = remote_macro_job.steps
     self.created_at = remote_macro_job.created_at
@@ -10,6 +11,7 @@ class PbmRemoteMacroJobSerializer
 
   def attributes
     { action: action,
+      name: name,
       uuid: uuid,
       steps: steps,
       created_at: created_at,
