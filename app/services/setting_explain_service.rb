c60727c1e751b1602c1c2431f7570bfb53d69d21
jiikko <n905i.1214@gmail.com>
2022-03-14 22:29:00 +0900

    コメント行を無視

--- app/services/setting_explain_service.rb
+++ app/services/setting_explain_service.rb
@@ -5,5 +5,18 @@ class SettingExplainService
 
   def execute
     return [] if @text == '' || @text.nil?
+    lines_exclude_prefix_space = @text.lines.map do |line|
+      line.gsub(/^\s+/, '').chomp
+    end
+
+    # remove comment
+    lines_exclude_prefix_space = lines_exclude_prefix_space.map do |line|
+      line.gsub(/^#+.*$/, '')
+    end
+
+    # remove empty lines
+    lines = lines_exclude_prefix_space.compact.reject { |x| x == '' }
+
+    lines
   end
 end
--- spec/services/setting_explain_service_spec.rb
+++ spec/services/setting_explain_service_spec.rb
@@ -12,5 +12,72 @@ describe SettingExplainService do
       let(:text) { "" }
       it { is_expected.to be_empty }
     end
+
+    context 'コメント行があるとき' do
+      context '1 row' do
+        let(:text) do
+          <<~TEXT
+          # macro ProconBypassMan::Plugin::Splatoon2::Macro::JumpToLeftKey, if_pressed: [:y, :b, :left]
+          TEXT
+        end
+        it { is_expected.to be_empty }
+      end
+
+      context '2 row with prefix spaces' do
+        let(:text) do
+          " # macro ProconBypassMan::Plugin::Splatoon2::Macro::JumpToLeftKey, if_pressed: [:y, :b, :left]\n" +
+          " # macro ProconBypassMan::Plugin::Splatoon2::Macro::JumpToLeftKey, if_pressed: [:y, :b, :left]"
+        end
+        it { is_expected.to be_empty }
+      end
+    end
+
+    context 'マクロ行があるとき' do
+      let(:text) do
+        <<~TEXT
+          macro ProconBypassMan::Plugin::Splatoon2::Macro::JumpToLeftKey, if_pressed: [:y, :b, :left]
+        TEXT
+      end
+
+      it do
+      end
+    end
+
+
+    context '連射の行があるとき' do
+      let(:text) do
+        <<~TEXT
+          flip :zr, if_pressed: :zr, force_neutral: :zl
+        TEXT
+      end
+    end
+
+    it do
+    end
+
+    context '複数の連射の行があるとき' do
+      let(:text) do
+        <<~TEXT
+          flip :zr, if_pressed: :zr, force_neutral: :zl
+          flip :zr, if_pressed: :zr, force_neutral: :zl
+          flip :a, if_pressed: :zr, force_neutral: :zl
+        TEXT
+      end
+
+      it do
+      end
+    end
+
+
+    context 'リマップの行があるとき' do
+      let(:text) do
+        <<~TEXT
+          remap :l, to: :zr
+        TEXT
+      end
+
+      it do
+      end
+    end
   end
 end
