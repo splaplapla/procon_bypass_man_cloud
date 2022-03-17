4a3d904d416a496eea1f8ffa2be94e599c786659
jiikko <n905i.1214@gmail.com>
2022-03-18 08:52:03 +0900

    remote macroを完了にするapiを実装する

---
+++ app/controllers/api/completed_pbm_remote_macro_jobs_controller.rb
@@ -0,0 +1,13 @@
+class Api::CompletedPbmRemoteMacroJobsController < Api::Base
+  def create
+    device = get_device
+    pbm_remote_macro_job = device.pbm_remote_macro_jobs.find_by!(uuid: params[:job_id])
+    pbm_remote_macro_job.update!(status: :processed)
+
+    render json: PbmRemoteMacroJobSerializer.new(pbm_remote_macro_job)
+  rescue ActiveRecord::RecordNotFound => e
+    render json: { errors: 'the remote_macro_job did not find' }, status: :bad_request
+  rescue ActiveModel::ValidationError => e
+    render json: { errors: e.model.errors.full_messages }, status: :bad_request
+  end
+end
