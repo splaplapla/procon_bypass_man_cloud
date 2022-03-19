6e2a0057e7da9b40d20a722cb01fb2e7f4783094
jiikko <n905i.1214@gmail.com>
2022-03-19 16:43:01 +0900

    削除とかできるようにする

--- app/controllers/streaming_services_controller.rb
+++ app/controllers/streaming_services_controller.rb
@@ -1,6 +1,9 @@
 class StreamingServicesController < ApplicationController
   def index
-    @streaming_services = current_user.streaming_services
+    @streaming_services = current_user.
+      streaming_services.
+      preload(:device, :remote_macro_group).
+      order(id: :asc)
   end
 
   def new
@@ -17,11 +20,22 @@ class StreamingServicesController < ApplicationController
   end
 
   def show
+    @streaming_service = current_user.streaming_services.find(params[:id])
+  end
+
+  def destroy
+    @streaming_service = current_user.streaming_services.find(params[:id]).destroy
+    redirect_to streaming_services_path, notice: "削除できました"
   end
 
   private
 
   def streaming_service_param
-    params.require(:streaming_service).permit(:name, :service_type, :remote_macro_group_id, :device_id)
+    params.require(:streaming_service).permit(
+      :name,
+      :service_type,
+      :remote_macro_group_id,
+      :device_id,
+    )
   end
 end
--- app/models/streaming_service.rb
+++ app/models/streaming_service.rb
@@ -1,5 +1,7 @@
 class StreamingService < ApplicationRecord
   belongs_to :user
+  belongs_to :device, optional: true
+  belongs_to :remote_macro_group, optional: true
 
   enum service_type: {
     youtube_live: 10,
