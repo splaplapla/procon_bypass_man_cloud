7ca7a0edb832e58562ae0f0ad1285b66eef72dba
jiikko <n905i.1214@gmail.com>
2022-03-23 17:40:32 +0900

    コメントからコマンドに変換するサービスクラスを実装する

--- app/models/streaming_service/youtube_live/chat_message.rb
+++ app/models/streaming_service/youtube_live/chat_message.rb
@@ -1,2 +1,5 @@
 class StreamingService::YoutubeLive::ChatMessage < Struct.new(:body, :author_channel_id, :author_name, :owner, :moderator, :published_at)
+  def to_trigger_word
+    body.remove(/^!/)
+  end
 end
