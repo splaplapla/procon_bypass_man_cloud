10b49b9dec62c7ad13a1ba93e3f6e34264a2200b
jiikko <n905i.1214@gmail.com>
2022-03-23 12:30:19 +0900

    use redirect

--- app/controllers/streaming_services/youtube_live/monitorings_controller.rb
+++ app/controllers/streaming_services/youtube_live/monitorings_controller.rb
@@ -3,13 +3,13 @@ class StreamingServices::YoutubeLive::MonitoringsController < ApplicationControl
     @streaming_service = current_user.streaming_services.find(params[:streaming_service_id])
     @streaming_service_account = @streaming_service.streaming_service_account
     @streaming_service_account.start_monitoring
-    head :ok
+    redirect_to streaming_service_youtube_live_path(@streaming_service, params[:youtube_live_id])
   end
 
   def destroy
     @streaming_service = current_user.streaming_services.find(params[:streaming_service_id])
     @streaming_service_account = @streaming_service.streaming_service_account
     @streaming_service_account.stop_monitoring
-    head :ok
+    redirect_to streaming_service_youtube_live_path(@streaming_service, params[:youtube_live_id])
   end
 end
