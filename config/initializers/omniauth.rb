0e10316e41224012846262b74e54ae6c8f5ebfa1
jiikko <n905i.1214@gmail.com>
2022-03-24 20:39:44 +0900

    use []

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV.fetch('GOOGLE_OAUTH2_CLIENT_ID'), ENV.fetch('GOOGLE_OAUTH2_SECRET'), {
+  provider :google_oauth2, ENV["GOOGLE_OAUTH2_CLIENT_ID"], ENV['GOOGLE_OAUTH2_SECRET'], {
     scope: 'userinfo.email, userinfo.profile, http://gdata.youtube.com',
     prompt: 'select_account',
     image_aspect_ratio: 'square',
