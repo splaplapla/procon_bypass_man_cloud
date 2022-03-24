d671ded79836f55a54df8f8bd7cfca864bc6509e
jiikko <n905i.1214@gmail.com>
2022-03-24 22:20:43 +0900

    order

--- app/controllers/remote_macro_groups_controller.rb
+++ app/controllers/remote_macro_groups_controller.rb
@@ -14,7 +14,7 @@ class RemoteMacroGroupsController < ApplicationController
 
   def show
     @remote_macro_group = current_user.remote_macro_groups.find(params[:id])
-    @remote_macros = @remote_macro_group.remote_macros
+    @remote_macros = @remote_macro_group.remote_macros.order(id: :acs)
   end
 
   def destroy
