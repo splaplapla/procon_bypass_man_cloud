7849798b7fb63cfcd9fdf10e3022288aa3caa5eb
jiikko <n905i.1214@gmail.com>
2022-04-17 18:31:03 +0900

    add spec

--- spec/models/device_spec.rb
+++ spec/models/device_spec.rb
@@ -9,4 +9,18 @@ RSpec.describe Device, type: :model do
       expect { device.save! }.to change { device.unique_key }.from(nil)
     end
   end
+
+  describe '#current_device_status_name' do
+    let(:device) { FactoryBot.create(:device) }
+
+    subject { device.current_device_status_name }
+
+    context 'current_device_status_idの実態が存在しない時' do
+      before do
+        device.update!(current_device_status_id: 0)
+      end
+
+      it { expect(subject).to eq('offline') }
+    end
+  end
 end
