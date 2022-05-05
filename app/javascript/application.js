4ff75691d47c7b072d8b0fd11d234e2644ad0732
jiikko <n905i.1214@gmail.com>
2022-05-05 15:09:06 +0900

    mv

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,16 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+import Rails from "@rails/ujs"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+import "bootstrap"
+
+Rails.start()
+ActiveStorage.start()
+
+$(function() { // eslint-disable-line
+  $('[data-bs-toggle="tooltip"]').tooltip(); // eslint-disable-line
+})
+
+import $ from 'jquery'
+window.$ = $
