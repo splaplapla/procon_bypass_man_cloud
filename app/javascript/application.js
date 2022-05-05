7825c58f2a5fdae96be18a338aac2ee5cb8909cc
jiikko <n905i.1214@gmail.com>
2022-05-05 17:02:22 +0900

    一旦commit

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,20 +3,23 @@
 import Rails from "@rails/ujs"
 import $ from 'jquery'
 // import "bootstrap"
-import * as bootstrap from 'bootstrap'
-window.bootstrap = bootstrap
-import "popper"
+import { createPopper } from '@popperjs/core';
 
 window.$ = $
 window.jQuery = $
+
+import * as bootstrap from 'bootstrap'
+window.bootstrap = bootstrap
 
 import "channels"
 
 Rails.start()
 
-
 $(function() { // eslint-disable-line
-  $('[data-bs-toggle="tooltip"]').tooltip(); // eslint-disable-line
+  // tooltip()とかくとエラーになるので、、、
+  // $('[data-bs-toggle="tooltip"]').tooltip(); // eslint-disable-line
+  const elem = document.querySelector('[data-bs-toggle="tooltip"]')
+  if(elem) { createPopper(elem, {}); }
 
   $("#global-flash").delay(3000).fadeOut('slow');
 })
