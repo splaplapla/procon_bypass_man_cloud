7d2340615beea292b725f2360b60cbbff8eb6380
jiikko <n905i.1214@gmail.com>
2022-05-05 15:49:46 +0900

    はい

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,16 +1,22 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
 import Rails from "@rails/ujs"
-import * as ActiveStorage from "@rails/activestorage"
+import $ from 'jquery'
+// import "bootstrap"
+import * as bootstrap from 'bootstrap'
+window.bootstrap = bootstrap
+import "popper"
+
+window.$ = $
+window.jQuery = $
+
 import "channels"
-import "bootstrap"
 
 Rails.start()
-ActiveStorage.start()
+
 
 $(function() { // eslint-disable-line
   $('[data-bs-toggle="tooltip"]').tooltip(); // eslint-disable-line
-})
 
-import $ from 'jquery'
-window.$ = $
+  $("#global-flash").delay(3000).fadeOut('slow');
+})
