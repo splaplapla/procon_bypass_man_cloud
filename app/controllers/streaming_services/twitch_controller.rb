0d5e101fe8ee014a62fc018bbea0a68c429955d6
jiikko <n905i.1214@gmail.com>
2022-06-12 19:02:50 +0900

    table

--- app/controllers/streaming_services/twitch_controller.rb
+++ app/controllers/streaming_services/twitch_controller.rb
@@ -11,5 +11,15 @@ class StreamingServices::TwitchController < StreamingServices::Base
     client = StreamingService::TwitchClient.new(@streaming_service_account)
     @live_stream = client.myself_live
     @twitch_my_user = client.myself
+    @macro_trigger_table = macro_trigger_table
+  end
+
+  private
+
+  def macro_trigger_table
+    @streaming_service.
+      remote_macro_group.
+      remote_macros.
+      flat_map { |x| x.trigger_word_list }.reduce({}) { |a, k| a[k] = true; a }
   end
 end
