a44a3800c03bee9ba4ab773d7740fab80b6b397c
jiikko <n905i.1214@gmail.com>
2022-07-16 19:01:24 +0900

    チャートを表示する

--- app/services/procon_performance_metric/read_service.rb
+++ app/services/procon_performance_metric/read_service.rb
@@ -1,10 +1,8 @@
-module ProconPerformanceMetric
-  class ReadService < ProconPerformanceMetric::Base
-    # @return [Array<ProconPerformanceMetricStruct>]
-    def execute(device_uuid: )
-      self.class.redis.lrange(device_uuid, 0, -1).map do |value|
-        deserialize(value)
-      end
+class ProconPerformanceMetric::ReadService < ProconPerformanceMetric::Base
+  # @return [Array<ProconPerformanceMetricStruct>]
+  def execute(device_uuid: )
+    self.class.redis.lrange(device_uuid, 0, -1).map do |value|
+      deserialize(value)
     end
   end
 end
