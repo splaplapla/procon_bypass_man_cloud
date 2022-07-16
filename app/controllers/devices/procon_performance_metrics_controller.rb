654b31e33c78ffd33d9127b2276fe2f7488d16f0
jiikko <n905i.1214@gmail.com>
2022-07-16 19:55:11 +0900

    はい

--- app/controllers/devices/procon_performance_metrics_controller.rb
+++ app/controllers/devices/procon_performance_metrics_controller.rb
@@ -14,6 +14,8 @@ class Devices::ProconPerformanceMetricsController < ApplicationController
     @read_write_time_data_list = [
       { data: metrics.map { |x| [x.timestamp, x.time_taken_max] }, name: '最大' },
       { data: metrics.map { |x| [x.timestamp, x.time_taken_p50] }, name: '中央値' },
+      { data: metrics.map { |x| [x.timestamp, x.time_taken_p95] }, name: 'P95' },
+      { data: metrics.map { |x| [x.timestamp, x.time_taken_p99] }, name: 'P99' },
     ]
     @bypass_data_list = [
       { data: metrics.map { |x| [x.timestamp, x.interval_from_previous_succeed_max] }, name: '最大' },
