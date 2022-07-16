30fe5ea42a2cd925169c76e9bce04c8c3d7143f5
jiikko <n905i.1214@gmail.com>
2022-07-16 20:36:45 +0900

    connection poolやっぱ使う

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,5 @@
 ActiveSupport::Reloader.to_prepare do
   redis = Redis.new(url: ENV['REDIS_URL'])
   redis_namespace = Redis::Namespace.new(:procon_performance_metric, redis: redis)
-  ProconPerformanceMetric::Base.redis = redis_namespace
+  ProconPerformanceMetric::Base.redis = ConnectionPool::Wrapper.new(size: 5, timeout: 3) { redis_namespace }
 end
