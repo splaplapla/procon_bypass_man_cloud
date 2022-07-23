4f43f58b0a2e594e61ec54e2040a697ebc15c28f
jiikko <n905i.1214@gmail.com>
2022-07-23 17:39:08 +0900

    add plan

--- app/models/user_plan.rb
+++ app/models/user_plan.rb
@@ -8,27 +8,32 @@ module UserPlan
     module CAPACITY
       PERFORMANCE_METRICS_RETENTION_HOURS = :performance_metrics_retention_hours
       MAX_SAVED_SETTINGS_SIZE = :max_saved_settings_size
+      MAX_DEVICES_SIZE = :max_devices_size
 
       DETAIL = {
         PLAN_FREE => {
           name: :free,
           PERFORMANCE_METRICS_RETENTION_HOURS => 2,
           MAX_SAVED_SETTINGS_SIZE => 3,
+          MAX_DEVICES_SIZE => 2,
         },
         PLAN_LIGHT => {
           name: :light,
           PERFORMANCE_METRICS_RETENTION_HOURS => 5,
           MAX_SAVED_SETTINGS_SIZE => 10,
+          MAX_DEVICES_SIZE => 3,
         },
         PLAN_STANDARD => {
           name: :standard,
           PERFORMANCE_METRICS_RETENTION_HOURS => 20,
           MAX_SAVED_SETTINGS_SIZE => 20,
+          MAX_DEVICES_SIZE => 4,
         },
         PLAN_PRO => {
           name: :pro,
           PERFORMANCE_METRICS_RETENTION_HOURS => 20,
           MAX_SAVED_SETTINGS_SIZE => 40,
+          MAX_DEVICES_SIZE => 10,
         },
       }
     end
