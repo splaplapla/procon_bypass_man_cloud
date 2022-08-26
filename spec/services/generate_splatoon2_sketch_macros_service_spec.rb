f88a601e91146e954410d27d1cea8add7c102eda
jiikko <n905i.1214@gmail.com>
2022-08-26 13:01:49 +0900

    短くする

--- spec/services/generate_splatoon2_sketch_macros_service_spec.rb
+++ spec/services/generate_splatoon2_sketch_macros_service_spec.rb
@@ -10,8 +10,12 @@ describe GenerateSplatoon2SketchMacrosService do
 
   it do
     expect(described_class.new(list_in_list: list_in_list).execute).to eq([
-      [[:pressing_a_for_0_5sec, :pressing_right_for_0_5sec], [:pressing_right_for_0_5sec], [:pressing_down_for_0_5sec]],
-      [[:pressing_left_for_0_5sec], [:pressing_a_for_0_5sec, :pressing_left_for_0_5sec], [:pressing_down_for_0_5sec]]
+      [
+        GenerateSplatoon2SketchMacrosService::MACRO_POINT, GenerateSplatoon2SketchMacrosService::MACRO_NOT_POINT, GenerateSplatoon2SketchMacrosService::MACRO_NEXT_LINE
+      ],
+      [
+        GenerateSplatoon2SketchMacrosService::MACRO_REVERSE_NOT_POINT, GenerateSplatoon2SketchMacrosService::MACRO_REVERSE_POINT, GenerateSplatoon2SketchMacrosService::MACRO_NEXT_LINE
+      ]
     ])
   end
 end
