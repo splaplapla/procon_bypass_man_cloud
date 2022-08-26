3cf5673a3e8c69451da1f4420c2e636414d1624a
jiikko <n905i.1214@gmail.com>
2022-08-26 12:53:43 +0900

    wait_for_0_5secする

--- app/services/generate_splatoon2_sketch_macros_service.rb
+++ app/services/generate_splatoon2_sketch_macros_service.rb
@@ -2,11 +2,11 @@
 class GenerateSplatoon2SketchMacrosService
   # 偶数. 右へ移動する
   MACRO_POINT = [:pressing_a_for_0_5sec, :pressing_right_for_0_5sec]
-  MACRO_NOT_POINT = [:pressing_right_for_0_5sec]
+  MACRO_NOT_POINT = [:pressing_right_for_0_5sec, :wait_for_0_5sec]
 
   # 奇数. 左へ移動する
   MACRO_REVERSE_POINT = [:pressing_a_for_0_5sec, :pressing_left_for_0_5sec]
-  MACRO_REVERSE_NOT_POINT = [:pressing_left_for_0_5sec]
+  MACRO_REVERSE_NOT_POINT = [:pressing_left_for_0_5sec, :wait_for_0_5sec]
 
   MACRO_NEXT_LINE = [:pressing_down_for_0_5sec]
 
