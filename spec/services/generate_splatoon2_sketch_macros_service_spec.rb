4d07e7c4550a0299cfde780457871646b63aa752
jiikko <n905i.1214@gmail.com>
2022-08-26 11:40:02 +0900

    ooops

--- spec/services/generate_splatoon2_sketch_macros_service_spec.rb
+++ spec/services/generate_splatoon2_sketch_macros_service_spec.rb
@@ -10,8 +10,8 @@ describe GenerateSplatoon2SketchMacrosService do
 
   it do
     expect(described_class.new(list_in_list: list_in_list).execute).to eq([
-      [[:pressing_a_for_1sec, :pressing_right_for_1sec], [:pressing_right_for_1sec], [:pressing_down_for_1sec]],
-      [[:pressing_left_for_1sec], [:pressing_a_for_1sec, :pressing_left_for_1sec], [:pressing_down_for_1sec]]
+      [[:pressing_a_for_0_5sec, :pressing_right_for_0_5sec], [:pressing_right_for_0_5sec], [:pressing_down_for_0_5sec]],
+      [[:pressing_left_for_0_5sec], [:pressing_a_for_0_5sec, :pressing_left_for_0_5sec], [:pressing_down_for_0_5sec]]
     ])
   end
 end
