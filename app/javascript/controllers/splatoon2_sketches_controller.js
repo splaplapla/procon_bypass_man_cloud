702e83596cafd0d7fca8a70e8bb8ddfa83698d4c
jiikko <n905i.1214@gmail.com>
2022-08-28 18:41:31 +0900

    stimlasjsのコードをlintとformaterにかける

--- app/javascript/controllers/splatoon2_sketches_controller.js
+++ app/javascript/controllers/splatoon2_sketches_controller.js
@@ -1,15 +1,15 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus';
 import axios from 'axios';
 
 // Connects to data-controller="splatoon2-sketches"
 export default class extends Controller {
   connect() {
-    const imagePath = this.data.get('imagePathValue')
+    const imagePath = this.data.get('imagePathValue');
     this.imageElement = this.element;
-    axios.get(imagePath).then(this._processResponse.bind(this))
+    axios.get(imagePath).then(this._processResponse.bind(this));
   }
 
   _processResponse(response) {
-    this.imageElement.src = `data:${response.data.image_data}`
+    this.imageElement.src = `data:${response.data.image_data}`;
   }
 }
