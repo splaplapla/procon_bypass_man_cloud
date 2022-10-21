3f86f3ec117f9439f561f2fcbdf2a07e2ba3473c
jiikko <n905i.1214@gmail.com>
2022-10-22 01:26:28 +0900

    ドット打ちの色判定をRGBで行う

--- app/services/generate_splatoon2_sketch_binarization_list_service.rb
+++ app/services/generate_splatoon2_sketch_binarization_list_service.rb
@@ -1,19 +1,4 @@
 class GenerateSplatoon2SketchBinarizationListService
-  COLOR_BLACKS = Set.new([
-    "black",
-    "#2C1585",
-    "7957F0",
-    "gray1",
-    "gray2",
-    "gray3",
-    "gray4",
-    "gray5",
-    "gray6",
-    "gray7",
-    "gray8",
-    "gray9",
-  ])
-
   # @param [File] 2値化した画像のファイル
   def initialize(file: )
     @file = file
@@ -22,10 +7,27 @@ class GenerateSplatoon2SketchBinarizationListService
   # @return [Array<Array<Boolean>>] trueが黒で、falseが白
   def execute
     img = Magick::ImageList.new(@file.open)
+    img_depth = img.depth
     dots = img.rows.times.map { |y|
       img.columns.times.map { |x|
-        COLOR_BLACKS.include?(img.pixel_color(x, y).to_color)
+        color = img.pixel_color(x, y).to_color(Magick::AllCompliance, false, img_depth, true)
+        # Rails.logger.info "color: #{color}"
+        is_black?(color)
       }
     }
+  end
+
+  private
+
+  # @return [[Integer, Integer, Integer]] RGB
+  def to_rgb(color)
+    hex = color.remove(/^#/)
+    r, g, b = hex[0..1], hex[2..3], hex[4..5]
+    [r, g, b].map(&:hex)
+  end
+
+  # @return [Boolean]
+  def is_black?(color)
+    to_rgb(color).uniq.first < 120
   end
 end
