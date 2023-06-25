aa6bbabfaa6535792e2c658196043610b2dd25a1
jiikko <n905i.1214@gmail.com>
2023-06-25 15:50:15 +0900

    デバイス詳細画面起動時の処理をstimulasjsで書き換える

---
+++ app/javascript/controllers/device_onload_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus"
+
+// 時間的凝集
+// Connects to data-controller="device-onload"
+export default class extends Controller {
+  static values = { pingUrl: String, pbmVersionUrl: String }
+
+  connect() {
+    this.pingDevice();
+    this.getDevicePbmVersion();
+  }
+
+  pingDevice() {
+    if (this.hasPingUrlValue) {
+      let url = this.pingUrlValue;
+      fetch(url, { method: 'POST' });
+    }
+  }
+
+  getDevicePbmVersion() {
+    if (this.hasPbmVersionUrlValue) {
+      let url = this.pbmVersionUrlValue;
+      fetch(url);
+    }
+  }
+}
